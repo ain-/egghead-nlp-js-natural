f819226df029ebf1cb6ab27d2eb427297b35181d
Ain <ain.salula@gmail.com>
2017-03-18 23:57:58 +0200

    tagging (broken)

---
+++ 05.js
@@ -0,0 +1,17 @@
+var natural = require('natural');
+var Tagger = natural.BrillPOSTagger;
+
+var myString = "Lys soldered the beautiful jewelry pieces".split(" ");
+
+var baseFolder = "./node_modules/natural/lib/natural/brill_pos_tagger";
+var rules = baseFolder + "/data/English/tr_from_posjs.txt";
+var lexicon = baseFolder + "/data/English/lexicon_from_posjs.json";
+var defaultCategory = "N";
+
+var tagger = new Tagger(lexicon, rules, defaultCategory, function(err) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(tagger.tag(myString));
+  }
+});
